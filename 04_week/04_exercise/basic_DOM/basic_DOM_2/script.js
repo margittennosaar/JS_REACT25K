4a883f72b0e5478c9a217c2f7e0ec88583f77b03
Margit Tennosaar <margit.tennosaar@bc.fi>
2025-02-03 14:25:39 +0200

    add second task

---
+++ 04_week/04_exercise/basic_DOM/basic_DOM_2/script.js
@@ -0,0 +1,9 @@
+/*
+Use an HTML file with an <input> field, a <button>, and a <ul> list with the ID fruitList.
+Write JavaScript to:
+- Retrieve the value from the input field when the button is clicked.
+- Create a new <li> element and set its text content to the input field's value.
+- Append the <li> element to the list.
+- Clear the input field after adding the fruit.
+*/
+
